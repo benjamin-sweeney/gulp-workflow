10ee12b410b519a938287f55a135779d7fa423d6
Ben Sweeney <benjamin.sweeney.r@gmail.com>
2015-06-08 09:52:00 -0500

    Minification and success/error notifications added.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,39 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp 		= require('gulp'),
+    sass        = require('gulp-sass'),
+    rename      = require('gulp-rename'),
+    cssmin      = require('gulp-minify-css'),
+    prefix      = require('gulp-autoprefixer'),
+    size        = require('gulp-size'),
+    plumber     = require('gulp-plumber'),
+    notify      = require('gulp-notify');
 
-//Compile SASS to CSS
-gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
-        .pipe(sass({
-            errLogToConsole: true
-        }))
-        .pipe(gulp.dest('css/'));
+gulp.task('sass', function() {
+    var onError = function(err) {
+      notify.onError({
+          title:    "Gulp",
+          subtitle: "Failure!",
+          message:  "Error: <%= error.message %>",
+          sound:    "Beep"
+      })(err);
+      this.emit('end');
+  };
+
+  return gulp.src('sass/**/*.scss')
+    .pipe(plumber({errorHandler: onError}))
+    .pipe(sass({
+    	errLogToConsole: true
+    }))
+    .pipe(size({ gzip: true, showFiles: true }))
+    .pipe(prefix())
+    .pipe(gulp.dest('./css/'))
+    .pipe(cssmin())
+    .pipe(size({ gzip: true, showFiles: true }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./css/'))
+    .pipe(notify("Compile and minify complete"))
 });
 
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+    gulp.watch('sass/**/*.scss',['sass']);
 });
